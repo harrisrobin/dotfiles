6df4be64a466cd20962fd6647558b12e484a3732
Harris Robin Kalash <hello@harris.dev>
2022-04-17 02:35:32 -0700

    first commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,15 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+
+-- plugin configurations
+require "plugin.lualine"
+require "plugin.treesitter"
+require "plugin.telescope"
+require "plugin.ts-autotag"
+require "plugin.autopairs"
+require "plugin.cmp"
+
+-- Color scheme
+vim.cmd "colorscheme ayu"
+
---
+++ .config/nvim/lua/plugin/telescope.lua
@@ -0,0 +1,27 @@
+local status, telescope = pcall(require, "telescope")
+if (not status) then return end
+
+local keymap = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+
+keymap("n", ";f", ":lua require('telescope.builtin').find_files()<cr>", opts)
+keymap("n", ";r", ":lua require('telescope.builtin').live_grep()<cr>", opts)
+keymap("n", "\\", ":lua require('telescope.builtin').buffers()<cr>", opts)
+keymap("n", ";;", ":lua require('telescope.builtin').help_tags()<cr>", opts)
+
+function telescope_buffer_dir()
+  return vim.fn.expand('%:p:h')
+end
+
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+
+telescope.setup{
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close
+      },
+    },
+  }
+}
